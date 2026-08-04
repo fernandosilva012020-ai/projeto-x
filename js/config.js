6079727681e3fc872d3691e209ea23eed599c1ae
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-04 16:23:35 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://jddgebxqwmxngfitcftb.supabase.co";
 
+const SUPABASE_KEY = "SUA_PUBLISHABLE_KEY";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
