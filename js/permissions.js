58d93dcad885ef5ad5798d3dfe0fe8c99442b279
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-04 21:12:16 -0700

    Update permissions.js

--- js/permissions.js
+++ js/permissions.js
@@ -1,1 +1,33 @@
+async function verificarPlano() {
 
+    const { data: sessionData } = await window.supabaseClient.auth.getSession();
+
+    if (!sessionData.session) {
+        window.location.href = "login.html";
+        return;
+    }
+
+
+    const user = sessionData.session.user;
+
+
+    const { data: profile, error } = await window.supabaseClient
+        .from("profiles")
+        .select("plano,status")
+        .eq("id", user.id)
+        .single();
+
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+
+    console.log("Plano:", profile.plano);
+    console.log("Status:", profile.status);
+
+
+    return profile;
+
+}
