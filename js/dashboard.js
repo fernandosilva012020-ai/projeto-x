e22f0b21819c417dab926f76df08ed93f81cddc9
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-05 06:56:21 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,61 @@
+async function carregarDashboard() {
 
+    const { data } = await window.supabaseClient.auth.getSession();
+
+
+    if (!data.session) {
+        return;
+    }
+
+
+    const user = data.session.user;
+
+
+
+    // GRUPOS
+
+    const { count: grupos } = await window.supabaseClient
+        .from("groups")
+        .select("*", { count: "exact", head: true })
+        .eq("user_id", user.id);
+
+
+
+    document.getElementById("totalGrupos").innerText =
+        grupos || 0;
+
+
+
+    // TEMPLATES
+
+    const { count: templates } = await window.supabaseClient
+        .from("templates")
+        .select("*", { count: "exact", head: true })
+        .eq("user_id", user.id);
+
+
+
+    document.getElementById("totalTemplates").innerText =
+        templates || 0;
+
+
+
+
+    // CAMPANHAS
+
+    const { count: campanhas } = await window.supabaseClient
+        .from("campaigns")
+        .select("*", { count: "exact", head: true })
+        .eq("user_id", user.id);
+
+
+
+    document.getElementById("totalCampanhas").innerText =
+        campanhas || 0;
+
+
+
+}
+
+
+carregarDashboard();
