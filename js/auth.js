ecd47089fd3ca419ea51046d8a85ed7f5a65476b
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-04 17:04:41 -0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,2 +1,24 @@
-console.log("Projeto X conectado ao Auth");
-console.log(supabaseClient);
+async function cadastrar() {
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+
+    const { data, error } = await supabaseClient.auth.signUp({
+        email: email,
+        password: senha
+    });
+
+
+    if (error) {
+        alert(error.message);
+        return;
+    }
+
+
+    alert("Conta criada com sucesso!");
+
+    console.log(data);
+
+}
