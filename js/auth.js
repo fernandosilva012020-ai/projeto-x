f77df03fabfd5b5c44495b9cbe7f0df5e50d0d06
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-04 19:06:07 -0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 async function cadastrar() {
-
+alert("Função cadastro iniciou");
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
