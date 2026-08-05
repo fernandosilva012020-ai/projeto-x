bbbdce1f46bd0e169625194a07c1da52cd1e30a4
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-04 18:05:26 -0700

    Update protect.js

--- js/protect.js
+++ js/protect.js
@@ -1,1 +1,16 @@
+async function protectPage() {
 
+    const { data } = await window.supabaseClient.auth.getSession();
+
+
+    if (!data.session) {
+
+        window.location.href = "login.html";
+        return;
+
+    }
+
+
+    console.log("Usuário autenticado:", data.session.user.email);
+
+}
