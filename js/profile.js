b5f6a8f0efe24d8ea0449b977cc4c8e206f2cb70
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-04 19:36:36 -0700

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,33 @@
+async function carregarPerfil() {
 
+    const { data: sessionData } = await window.supabaseClient.auth.getSession();
+
+    const user = sessionData.session?.user;
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+
+    const { data, error } = await window.supabaseClient
+        .from("profiles")
+        .select("*")
+        .eq("id", user.id)
+        .single();
+
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+
+    document.getElementById("nomeUsuario").innerHTML = data.nome;
+    document.getElementById("planoUsuario").innerHTML = data.plano;
+    document.getElementById("statusUsuario").innerHTML = data.status;
+
+}
+
+
+carregarPerfil();
