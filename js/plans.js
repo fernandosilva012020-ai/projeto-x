ef7988b264fae9a48eeca0f5e98bf5d4bdd76346
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-04 21:46:36 -0700

    Update plans.js

--- js/plans.js
+++ js/plans.js
@@ -1,1 +1,60 @@
+async function carregarPlanos() {
 
+
+    const { data: planos, error } = await window.supabaseClient
+        .from("plans")
+        .select("*")
+        .eq("ativo", true);
+
+
+
+    if (error) {
+
+        console.log(error);
+        return;
+
+    }
+
+
+
+    const area = document.getElementById("listaPlanos");
+
+
+    area.innerHTML = "";
+
+
+
+    planos.forEach(plano => {
+
+
+
+        area.innerHTML += `
+
+        <a href="#" onclick="escolherPlano('${plano.id}')">
+
+        💳
+
+        <span>
+        ${plano.nome}
+        </span>
+
+
+        <small>
+        R$ ${plano.preco}
+        </small>
+
+
+        <small>
+        ${plano.recursos}
+        </small>
+
+
+        </a>
+
+        `;
+
+
+    });
+
+
+}
