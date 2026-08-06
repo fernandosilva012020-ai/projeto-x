42f518a978db734e3dfde7a865160f5a34ba1ae5
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-05 17:31:53 -0700

    Update dashboard.js

--- js/js/dashboard.js
+++ js/js/dashboard.js
@@ -1,1 +1,76 @@
+// =================================
+// Projeto X - Dashboard
+// Carregar dados do usuário
+// =================================
 
+
+async function carregarDashboard() {
+
+
+    const { data } = await window.supabaseClient.auth.getSession();
+
+
+    if (!data.session) {
+
+        window.location.href = "login.html";
+
+        return;
+
+    }
+
+
+    const user = data.session.user;
+
+
+
+    // Busca perfil
+
+    const { data: profile, error } = await window.supabaseClient
+
+        .from("profiles")
+
+        .select("nome, plano, status")
+
+        .eq("id", user.id)
+
+        .single();
+
+
+
+    if (error) {
+
+        console.log(error.message);
+
+        return;
+
+    }
+
+
+
+    // Nome
+
+    document.getElementById("nomeUsuario").innerHTML =
+        profile.nome;
+
+
+
+    // Plano
+
+    document.getElementById("planoUsuario").innerHTML =
+        profile.plano;
+
+
+
+    // Status
+
+    document.getElementById("statusUsuario").innerHTML =
+        profile.status;
+
+
+}
+
+
+
+// iniciar
+
+carregarDashboard();
