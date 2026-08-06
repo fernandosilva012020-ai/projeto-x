ae468d60dd0292f2e9bf528f64a6a866b501356f
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-05 20:00:54 -0700

    Update sbcforge.js

--- js/js/sbcforge.js
+++ js/js/sbcforge.js
@@ -1,60 +1,7 @@
-console.log("SBCForge carregado");
+console.log("SBCFORGE JS OK");
 
+function criarSbc() {
 
-async function criarSbc() {
-
-
-    const nome = document.getElementById("nomeSbc").value;
-
-    const requisitos = document.getElementById("requisitosSbc").value;
-
-
-    if (!nome) {
-
-        alert("Digite o nome do SBC");
-
-        return;
-
-    }
-
-
-    const { data } = await window.supabaseClient.auth.getSession();
-
-
-    if (!data.session) {
-
-        alert("Usuário não conectado");
-
-        return;
-
-    }
-
-
-    const user = data.session.user;
-
-
-    const { error } = await window.supabaseClient
-        .from("sbcs")
-        .insert({
-
-            user_id: user.id,
-            nome: nome,
-            requisitos: requisitos
-
-        });
-
-
-    if (error) {
-
-        console.log(error);
-
-        alert("Erro ao criar SBC");
-
-        return;
-
-    }
-
-
-    alert("SBC criado com sucesso!");
+    alert("Botão funcionando!");
 
 }
