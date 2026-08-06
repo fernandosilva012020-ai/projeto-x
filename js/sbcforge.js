10e94a16e8797cd6c74099dd5b1a25861c72774b
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-05 21:02:44 -0700

    Update sbcforge.js

--- js/sbcforge.js
+++ js/sbcforge.js
@@ -1,1 +1,70 @@
+// =================================
+// Projeto X - SBCForge
+// Controle da interface
+// =================================
 
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    const modal = document.getElementById("modalSbc");
+
+    const abrir = document.getElementById("novoSbc");
+
+    const fechar = document.getElementById("fecharModal");
+
+    const cancelar = document.getElementById("cancelarSbc");
+
+
+
+    if (abrir && modal) {
+
+        abrir.addEventListener("click", () => {
+
+            modal.style.display = "flex";
+
+        });
+
+    }
+
+
+
+    if (fechar) {
+
+        fechar.addEventListener("click", () => {
+
+            modal.style.display = "none";
+
+        });
+
+    }
+
+
+
+    if (cancelar) {
+
+        cancelar.addEventListener("click", () => {
+
+            modal.style.display = "none";
+
+        });
+
+    }
+
+
+
+    window.addEventListener("click", (event) => {
+
+
+        if (event.target === modal) {
+
+            modal.style.display = "none";
+
+        }
+
+
+    });
+
+
+
+});
