7140952b0387566082cc44029b464830f1d461d5
fernandosilva012020-ai <fernandosilva012020@gmail.com>
2026-08-07 20:24:20 -0700

    Update router.js

--- js/core/router.js
+++ js/core/router.js
@@ -11,24 +11,23 @@ ProjetoX.router = {
         const area = document.querySelector("#app-view");
 
         if (!area) {
-            console.log("app-view não encontrada");
+            console.error("app-view não encontrada");
             return;
         }
 
         if (modulo === "SBCForge") {
 
             if (
-                window.SBCForge &&
-                typeof window.SBCForge.abrir === "function"
+                ProjetoX.SBCForge &&
+                typeof ProjetoX.SBCForge.abrir === "function"
             ) {
-                window.SBCForge.abrir();
-            } else {
-                area.innerHTML = `
-                    <h2>⚠️ SBCForge ainda não carregou.</h2>
-                `;
-
-                console.error("SBCForge não carregado.");
+                ProjetoX.SBCForge.abrir();
+                return;
             }
+
+            area.innerHTML = `
+                <h2>⚠️ SBCForge não carregou.</h2>
+            `;
 
         }
 
